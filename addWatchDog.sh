3623321f8c527908c4611df21ff033537858c7e0
fcm19950329 <fcm19950329@163.com>
2018-11-29 21:40:10 +0800

    新增以行为单位进行数据的去重，其中每行中有空格
    以行为单位进行数据的去重，其中每行中有空格

--- addWatchDog.sh
+++ addWatchDog.sh
@@ -1,23 +1,38 @@
-#！/bin/bash
+#!/bin/bash
 addWatchDog()
 {
-        oldwd=`pwd`
-        base_dir="/usr/local/watch/check_process.txt"
-        content="$oldwd/ins.sh start :pid.file"
-        flag=0
-        touch t1.txt
-        cat $base_dir > t1.txt
-         while read line
+        base_dir="he.txt"
+        i=0
+        touch t2.txt
+        cat $base_dir > t2.txt
+        while read line
+                do
+                        arr[${i}]=`echo ${line}`
+                        num[${i}]=0
+                        ((++i))
+
+                done<t2.txt
+        #去重
+        for(( m=0;m<${#arr[@]};m++))
         do
-            new_str=$(echo $line | cut -d " " -f1)
-            if [ "$new_str" = "$oldwd/ins.sh" ];then
-                    let "flag=flag+1"
-                    echo $flag
-            fi
-         done<t1.txt
-         rm /tmp/t1.txt -f
-         echo $flag
-         if [ $flag = 0 ];then                                                                                                                                                      echo -e $content >> $base_dir
-         fi
+                echo ${arr[m]}
+                for(( n=m+1;n<${#arr[@]};n++))
+                do
+                        if [ "${arr[m]}" = "${arr[n]}" ];then
+                                num[n]=1
+                        fi
+                done
+        done
+        rm -rf he.txt
+        touch he.txt
+        for(( j=0;j<${#arr[@]};j++))
+        do
+                echo ${num[j]}
+                if [ "${num[j]}" = "0" ];then
+                        echo ${arr[j]} >> he.txt
+                fi
+        done
+
 }
+
 addWatchDog
